36adf1ba1377409110cd0096efeb08b8ba20e23d
Susindhar M <suriyamanivasagan12@gmail.com>
2026-08-07 12:57:13 +0200

    updated controller codes 2

--- pico_adapter_to_fpga.v
+++ pico_adapter_to_fpga.v
@@ -1,11 +1,11 @@
 
-
+// Type never assigned
 
 module pico_adapter_to_fpga (
     input  wire [15:0] ui_in,    // Dedicated inputs
     output wire [15:0] uo_out,   // Dedicated outputs
-    input  wire [15:0] uio_in,   // IOs: Input path
-    output wire [15:0] uio_out,  // IOs: Output path
+    input  wire [3:0] uio_in,   // IOs: Input path
+    output wire [11:0] uio_out,  // IOs: Output path
     output wire [15:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire        MEM_VALID,
     input  wire        MEM_INSTR,
@@ -18,63 +18,74 @@ module pico_adapter_to_fpga (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+
+    // State machine states
     localparam IDLE  = 4'b0000;
-    localparam AR_1  = 4'b0001;
-    localparam AR_2  = 4'b0010;
+    localparam AR_1  = 4'b0001;    
     localparam RD_1  = 4'b0011;
-    localparam RD_2  = 4'b0011;
+    localparam RD_2  = 4'b0111;
     localparam WD_1  = 4'b0100;
     localparam WD_2  = 4'b0101;
-    localparam REG_RD= 2'b00;
-    localparam REG_W = 2'b00;
-    localparam MEM_RD= 2'b00;
-    localparam MEM_W = 2'b00;
-    wire        MEM_VALID;
-	wire        MEM_INSTR;
-	wire [31:0] MEM_ADDR ;
-	wire [31:0] MEM_WDATA;
-	wire [ 3:0] MEM_WSTRB;
-    reg         mem_ready;
-	reg [31:0]  mem_rdata;
-	wire [1:0]       type; 
-    reg [1:0] state, next;
+    
+    
+    localparam MEM_RD= 1'b0;
+    localparam MEM_W = 1'b1;
+
+    // wire        MEM_VALID;
+	// wire        MEM_INSTR;
+	// wire [31:0] MEM_ADDR ;
+	// wire [31:0] MEM_WDATA;
+	// wire [ 3:0] MEM_WSTRB;
+    // reg         mem_ready;
+	// reg [31:0]  mem_rdata;
+	wire        type; 
+    reg [3:0] state, next;
     wire FPGA_READY;
 
     reg [15:0] uo_out_reg;   // Dedicated outputs
     reg [15:0] uio_out_reg;  // IOs: Output path
     assign uo_out       = uo_out_reg;
-    assign uio_out      = uio_out_reg;
-    assign uio_oe[3:0]  = 8'b0000;//setting the uio lower part to input
-    assign uio_oe[15:4] = 8'b111111111111;//setting the uio higher part to output
-    assign uio_out[3]   = MEM_INSTR;
-    assign uio_out[7:3] = MEM_WSTRB;
-    assign uio_out[2]   = MEM_VALID;
-    assign uio_out[1:0] = type;
+    
+    assign uio_oe[3:0]  = 4'b0000;//setting the uio lower part to input
+    assign uio_oe[15:4] = 12'b111111111111;//setting the uio higher part to output
+    assign uio_out[2]   = MEM_INSTR;
+    assign uio_out[6:3] = MEM_WSTRB;
+    assign uio_out[1]   = MEM_VALID;  
+    assign uio_out[0] = type;
     assign FPGA_READY   = uio_in[0];
+
+    assign type = (MEM_WSTRB == 4'b0000) ? 1'b0 : 1'b1;
+
     always @(posedge clk)
         if(!rst_n)
             state<=IDLE;
         else 
             state<=next;
 
+
+  
+  
+    always @(posedge clk) begin
+        if (!rst_n)
+            mem_rdata <= 32'd0;
+        else begin
+            if (state == AR_1 && FPGA_READY)
+                mem_rdata[15:0] <= ui_in;
+    
+            if (state == RD_1)
+                mem_rdata[31:16] <= ui_in;
+        end
+    end
+    
     always @(*) begin
         next = IDLE;
         mem_ready   = 0;
-        mem_rdata   = 0;
-        uo_out_reg  = 0;
+       
+        uo_out_reg      = 0;
                         
         case(state)   
         IDLE: begin
-            if(MEM_VALID) begin
-            //send the address, can be register address or memory address
-                uo_out_reg =  MEM_ADDR[15:0];
-                //if register address, it only needs 5 bits, therefore it can go to
-                // read or write first state and get or send the 16 least signigicant
-                if     (type == REG_RD)
-                    next   =  RD_1;
-                else if(type == REG_W)
-                    next   =  WD_1;  
-                else if(type == MEM_W || MEM_RD)
+            if(MEM_VALID) begin            
                     next   =  AR_1;               
             end        
         else
@@ -82,10 +93,10 @@ module pico_adapter_to_fpga (
         end
         //read the upper half of the address in case of memory request
         AR_1: begin
-            uo_out_reg =  MEM_ADDR[31:15];            
+            uo_out_reg =  MEM_ADDR[31:16];            
             if(type == MEM_RD) begin
                 if(FPGA_READY) begin
-                    mem_rdata[15:0]  = ui_in;
+                   
                     next             =  RD_1;
                 end 
                 else
@@ -95,7 +106,7 @@ module pico_adapter_to_fpga (
                 next                 = WD_1;             
       end      
        RD_1: begin
-            mem_rdata[31:0]          = ui_in;
+            
             mem_ready                = 1;
             next                     = IDLE;
        end
