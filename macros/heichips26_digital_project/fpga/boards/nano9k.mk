49180c69bb59e81dbe67b683acab6bff7a17bdf6
pouriahassani <poria19964@gmail.com>
2026-09-10 11:16:21 +0200

    clean up the old files

--- macros/heichips26_digital_project/fpga/boards/nano9k.mk
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2026 The HeiChips Contributors
-# SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
-
-ARCH         := gowin
-GOWIN_DEVICE := GW1NR-LV9QN88PC6/I5
-GOWIN_FAMILY := GW1N-9C
-
-OPENFPGALOADER_BOARD := tangnano9k
-OPENFPGALOADER_FLAGS :=
